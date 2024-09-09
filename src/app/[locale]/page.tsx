f68aaedd4727d4ef050385b429079d5c2f440471
ethan <wp_wang@manka.co.jp>
2024-09-09 16:17:14 +0900

    init

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,16 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { FormExample } from "./form";
 export default function Home() {
   return (
-    <div className="flex p-4 gap-4">
-      <Input />
-      <Button>Hello</Button>
+    <div className="flex flex-col gap-4">
+      <div className="flex p-4 gap-4">
+        <Input />
+        <Button>Hello</Button>
+      </div>
+      <div>
+        <FormExample />
+      </div>
     </div>
   );
 }
