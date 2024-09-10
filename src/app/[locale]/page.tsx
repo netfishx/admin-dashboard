7056d3b4ba15865fceffa04b687136d69dad6300
ethan <wp_wang@manka.co.jp>
2024-09-10 09:11:43 +0900

    init

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,16 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { headers } from "next/headers";
 import { FormExample } from "./form";
+
 export default function Home() {
+  const FALLBACK_IP_ADDRESS = '0.0.0.0'
+  console.info(headers().get('x-forwarded-for'))
+  const array = headers().get('x-forwarded-for')?.split(',')[0]?.split(':')
+  const ip = array?.[array.length - 1] ?? FALLBACK_IP_ADDRESS
   return (
     <div className="flex flex-col gap-4">
+      {ip}
       <div className="flex p-4 gap-4">
         <Input />
         <Button>Hello</Button>
