96d4583bae5f4aaa686ac18fab90ff2465b087d6
ethan <wp_wang@manka.co.jp>
2024-09-25 12:51:19 +0900

    init

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -5,7 +5,9 @@ import { Toaster } from "@/components/ui/sonner";
 import { Provider } from "jotai";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
+import { ViewTransitions } from "next-view-transitions";
 import type { ReactNode } from "react";
+
 export const metadata: Metadata = {
   title: "管理后台",
 };
@@ -19,19 +21,21 @@ export default async function RootLayout({
 }>) {
   const messages = await getMessages();
   return (
-    <html lang={locale}>
-      <body>
-        <Toaster
-          position="top-center"
-          richColors
-          expand
-          visibleToasts={1}
-          toastOptions={{ duration: 1000 }}
-        />
-        <NextIntlClientProvider messages={messages}>
-          <Provider>{children}</Provider>
-        </NextIntlClientProvider>
-      </body>
-    </html>
+    <ViewTransitions>
+      <html lang={locale}>
+        <body>
+          <Toaster
+            position="top-center"
+            richColors
+            expand
+            visibleToasts={1}
+            toastOptions={{ duration: 1000 }}
+          />
+          <NextIntlClientProvider messages={messages}>
+            <Provider>{children}</Provider>
+          </NextIntlClientProvider>
+        </body>
+      </html>
+    </ViewTransitions>
   );
 }
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,6 +5,7 @@ import { Time } from "@/app/[locale]/time";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Input as Password } from "@/components/ui/password";
+import { Link } from "next-view-transitions";
 import { headers } from "next/headers";
 import { Suspense } from "react";
 
@@ -17,6 +18,17 @@ export default function Home() {
   const ip = array?.[array.length - 1] ?? FALLBACK_IP_ADDRESS;
   return (
     <div className="flex flex-col gap-4 p-4">
+      <div className="flex gap-4">
+        <Button asChild variant="link">
+          <Link href="/about">About</Link>
+        </Button>
+        <Button asChild variant="link">
+          <Link href="/list">List</Link>
+        </Button>
+        <Button asChild variant="link">
+          <Link href="/search">Search</Link>
+        </Button>
+      </div>
       <div className="flex justify-end">
         <RefreshButton />
       </div>
