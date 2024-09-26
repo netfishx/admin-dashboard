15b1bfdd12d03fa7d2d9e6602db6a410809646fe
ethan <wp_wang@manka.co.jp>
2024-09-26 17:07:30 +0900

    init

--- src/app/[lang]/search/page.tsx
+++ src/app/[lang]/search/page.tsx
@@ -1,4 +1,6 @@
 import { SearchForm } from "@/app/[lang]/search/form";
+import { Button } from "@/components/ui/button";
+import { Link } from "next-view-transitions";
 
 export default async function SearchPage({
   searchParams,
@@ -7,6 +9,17 @@ export default async function SearchPage({
 
   return (
     <div className="flex flex-col gap-4 p-4">
+      <div className="flex gap-4">
+        <Button asChild variant="link">
+          <Link href="/about">About</Link>
+        </Button>
+        <Button asChild variant="link">
+          <Link href="/list">List</Link>
+        </Button>
+        <Button asChild variant="link">
+          <Link href="/search">Search</Link>
+        </Button>
+      </div>
       name: {name} |age: {age}
       <SearchForm />
     </div>
