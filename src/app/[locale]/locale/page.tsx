3be080bfe25404d66e9a706c83bd68b9d174d847
ethan <wp_wang@manka.co.jp>
2024-09-27 10:21:16 +0900

    init

--- src/app/[locale]/locale/page.tsx
+++ src/app/[locale]/locale/page.tsx
@@ -1,12 +1,16 @@
 import { ClientI18n } from "@/app/[locale]/locale/client-i18n";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { getI18n } from "@/locales/server";
+import { Link } from "next-view-transitions";
 import { use } from "react";
 
 export default function LocalePage() {
   return (
     <div>
       国际化测试
+      <div>
+        <Link href="/">返回首页</Link>
+      </div>
       <div>
         <ServerI18n />
       </div>
