df396d74fa560c8d0806b36ac19fd3d6d7614682
ethan <wp_wang@manka.co.jp>
2024-10-03 13:06:49 +0900

    init

---
+++ src/components/theme-provider.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import type { ThemeProviderProps } from "next-themes/dist/types";
+import { useEffect, useState } from "react";
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  const [mounted, setMounted] = useState<boolean>(false);
+  useEffect(() => {
+    setMounted(true);
+    return () => setMounted(false);
+  }, []);
+  return (
+    mounted && <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  );
+}
