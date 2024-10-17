da46cc92944d53621befca8c85f67210f1939303
ethan <wp_wang@manka.co.jp>
2024-10-17 13:36:03 +0900

    init

--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,5 @@
+import { withTimeout } from "es-toolkit/promise";
+
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 const TIMEOUT = 5000;
 
@@ -13,41 +15,35 @@ export async function request(
   config?: RequestInit,
   token?: string,
 ): Promise<Response> {
-  const controller = new AbortController();
-  const timeoutId = setTimeout(() => controller.abort(), TIMEOUT);
+  const fullUrl = new URL(url, BASE_URL);
 
-  try {
-    const fullUrl = new URL(url, BASE_URL);
+  const headers = new Headers({
+    ...config?.headers,
+    "Content-Type": "application/json",
+    "Accept-Language": nextHeaders?.get("Accept-Language") ?? "zh-CN",
+  });
+  if (nextHeaders) {
+    const array = nextHeaders
+      ?.get("x-forwarded-for")
+      ?.split(",")[0]
+      ?.split(":");
+    const ip = array?.[array.length - 1] ?? FALLBACK_IP_ADDRESS;
+    headers.set("X-Forwarded-For", ip);
+  }
+  if (token) {
+    headers.set("Authorization", `Bearer ${token}`);
+  }
 
-    const headers = new Headers({
-      ...config?.headers,
-      "Content-Type": "application/json",
-      "Accept-Language": nextHeaders?.get("Accept-Language") ?? "zh-CN",
-    });
-    if (nextHeaders) {
-      const array = nextHeaders
-        ?.get("x-forwarded-for")
-        ?.split(",")[0]
-        ?.split(":");
-      const ip = array?.[array.length - 1] ?? FALLBACK_IP_ADDRESS;
-      headers.set("X-Forwarded-For", ip);
-    }
-    if (token) {
-      headers.set("Authorization", `Bearer ${token}`);
-    }
-
-    const response = await fetch(fullUrl.toString(), {
+  const response = await withTimeout(() => {
+    return fetch(fullUrl, {
       ...config,
       headers,
-      signal: controller.signal,
     });
+  }, TIMEOUT);
 
-    if (!validateStatus(response.status)) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    return response;
-  } finally {
-    clearTimeout(timeoutId);
+  if (!validateStatus(response.status)) {
+    throw new Error(`HTTP error! status: ${response.status}`);
   }
+
+  return response;
 }
