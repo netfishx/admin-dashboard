b6a726f08a7c99a45ea2d91fe67abab80d97b160
ethan <wp_wang@manka.co.jp>
2024-10-18 09:25:49 +0900

    init

--- src/actions.ts
+++ src/actions.ts
@@ -56,7 +56,7 @@ export const incrementNumberAction = actionClient
 export async function loginAction(formData: FormData) {
   const username = formData.get("username");
   const password = formData.get("password");
-  if (username === "admin" && password === "123456") {
+  if (username && password) {
     await setSession({
       id: 123,
     });
