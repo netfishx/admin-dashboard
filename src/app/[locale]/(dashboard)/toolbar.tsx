f176f0074220ae2c06aec47e4e535f4f83de11b0
ethan <wp_wang@manka.co.jp>
2024-10-22 16:01:51 +0900

    init

---
+++ src/app/[locale]/(dashboard)/toolbar.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ChevronDown, HomeIcon, MoonStar, QrCode, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { useState } from "react";
+export function Toolbar() {
+  const [name, _setName] = useState("Serati Ma");
+  const { resolvedTheme: mode, setTheme } = useTheme();
+  return (
+    <div className="w-full h-10 flex flex-row justify-between border-b px-2">
+      <div className="flex flex-row gap-2 items-center text-sm">
+        <HomeIcon className="size-4" />
+        <span>/</span>
+        <span>首页</span>
+      </div>
+      <div className="flex flex-row gap-2 items-center">
+        <div
+          className="cursor-pointer rounded-full border-solid border-2 p-1"
+          onClick={() => {
+            setTheme(mode === "dark" ? "light" : "dark");
+          }}
+        >
+          {mode === "dark" ? (
+            <MoonStar className="size-4" />
+          ) : (
+            <Sun className="size-4" />
+          )}
+        </div>
+        <div className="cursor-pointer rounded-full border-solid border-2 p-1">
+          <QrCode className="size-4" />
+        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" size="sm">
+              {name}
+              <ChevronDown />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem>退出登录</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
+  );
+}
