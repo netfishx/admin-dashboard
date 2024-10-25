c6004574595c8157713c04b01658347134f95ab3
gareth <gareth_han@manka.co.jp>
2024-10-25 11:50:24 +0900

    feat: 开发注单列表

---
+++ src/app/[locale]/(dashboard)/reports/orderlist/page.tsx
@@ -0,0 +1,203 @@
+'use client'
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { useTranslations } from "next-intl";
+import DateRangeFilter from "./DateRangeFilter";
+const data = [
+  {
+    game: "百家乐01",
+    video: "https://www.baidu.com",
+    replay: "https://www.baidu.com",
+    supplierId: "1",
+    supplierName: "供应商01",
+    quota: 100,
+    percent: 10,
+  },
+  {
+    game: "百家乐02",
+    video: "https://www.baidu.com",
+    replay: "https://www.baidu.com",
+    supplierId: "1",
+    supplierName: "供应商01",
+    quota: 100,
+    percent: 10,
+  },
+  {
+    game: "百家乐03",
+    video: "https://www.baidu.com",
+    replay: "https://www.baidu.com",
+    supplierId: "1",
+    supplierName: "供应商01",
+    quota: 100,
+    percent: 10,
+  },
+  {
+    game: "百家乐04",
+    video: "https://www.baidu.com",
+    replay: "https://www.baidu.com",
+    supplierId: "1",
+    supplierName: "供应商01",
+    quota: 100,
+    percent: 10,
+  },
+  {
+    game: "百家乐05",
+    video: "https://www.baidu.com",
+    replay: "https://www.baidu.com",
+    supplierId: "1",
+    supplierName: "供应商01",
+    quota: 100,
+    percent: 10,
+  },
+];
+
+export default function Page() {
+  const t = useTranslations("report.orderlist");
+  const handleDateRangeChange = (date) => {
+    console.log(date, 'date');
+  }
+  return (
+    <div className="flex flex-col gap-2 w-full">
+      {/* 筛选条件 */}
+      <div className="flex flex-col gap-2 bg-background py-2 px-4">
+        {/* 第一行 */}
+        <div className="flex gap-4 items-center">
+          <div className="flex gap-2 items-center">
+            <Label className="shrink-0">{t("gametype")}</Label>
+            <Select defaultValue="1">
+              <SelectTrigger className="w-28">
+                <SelectValue placeholder={t("placeholder")} />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="1">百家乐</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="flex gap-2 items-center">
+            <Label className="shrink-0">{t("gamename")}</Label>
+            <Select defaultValue="1">
+              <SelectTrigger className="w-28">
+                <SelectValue placeholder={t("placeholder")} />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="1">百家乐</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="flex gap-2 items-center">
+            <Select>
+              <SelectTrigger className="w-28">
+                <SelectValue placeholder={t("bettingtime")} />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="1">百家乐</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+          <DateRangeFilter onChange={handleDateRangeChange} />
+          <div className="flex gap-2 items-center">
+            <Label className="shrink-0">{t("settlementstatus")}</Label>
+            <Select>
+              <SelectTrigger className="w-28">
+                <SelectValue placeholder={t("placeholderselect")} />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="1">百家乐</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+          <div className="flex gap-2 items-center">
+            <Label className="shrink-0">{t("ordernumber")}</Label>
+            <Input placeholder={t("placeholderinput")} />
+          </div>
+        </div>
+        {/* 第二行 */}
+        <div className="flex gap-4 items-center">
+          <div className="flex gap-2 items-center">
+            <Label className="shrink-0">{t("issuenumber")}</Label>
+            <Input placeholder={t("placeholderinput")} />
+          </div>
+          <div className="flex gap-2 items-center">
+            <Label className="shrink-0">{t("ministerID")}</Label>
+            <Input placeholder={t("placeholderinput")} />
+          </div>
+          <div className="flex gap-2 items-center">
+            <Label className="shrink-0">{t("memberID")}</Label>
+            <Input placeholder={t("placeholderinput")} />
+          </div>
+          <div className="flex gap-2 items-center">
+            <Label className="shrink-0">{t("agentID")}</Label>
+            <Input placeholder={t("placeholderinput")} />
+          </div>
+          <div className="flex gap-2 items-center">
+            <Label className="shrink-0">{t("roomeownerID")}</Label>
+            <Input placeholder={t("placeholderinput")} />
+          </div>
+        </div>
+        <div className="flex gap-2 justify-end items-center">
+          <Button>{t("search")}</Button>
+        </div>
+      </div>
+      
+      {/* 表格 */}
+      <div className="p-2 bg-background flex-1">
+        <div className="border rounded-sm">
+          <Table>
+            <TableHeader>
+              <TableRow className="bg-muted">
+                <TableHead>{t("name")}</TableHead>
+                <TableHead>{t("video")}</TableHead>
+                <TableHead>{t("replay")}</TableHead>
+                <TableHead>{t("supplierId")}</TableHead>
+                <TableHead>{t("supplierName")}</TableHead>
+                <TableHead>{t("quota")}</TableHead>
+                <TableHead>{t("percent")}</TableHead>
+                <TableHead className="w-24 text-center">
+                  {t("action")}
+                </TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {data.map((item) => (
+                <TableRow key={item.game}>
+                  <TableCell>{item.game}</TableCell>
+                  <TableCell>{item.video}</TableCell>
+                  <TableCell>{item.replay}</TableCell>
+                  <TableCell>{item.supplierId}</TableCell>
+                  <TableCell>{item.supplierName}</TableCell>
+                  <TableCell>{item.quota}</TableCell>
+                  <TableCell>{item.percent}</TableCell>
+                  <TableCell className="w-24 text-center">
+                    <Button
+                      variant="link"
+                      className="hover:no-underline hover:text-primary/80"
+                    >
+                      {t("edit")}
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+    </div>
+  );
+}
