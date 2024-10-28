7a56a348b4e3603638247c23089a39a247fac6e0
ethan <wp_wang@manka.co.jp>
2024-10-28 15:09:04 +0900

    init

---
+++ src/app/global-error.tsx
@@ -0,0 +1,20 @@
+"use client"; // Error boundaries must be Client Components
+
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <h2>Something went wrong!</h2>
+        <button type="button" onClick={() => reset()}>
+          Try again
+        </button>
+      </body>
+    </html>
+  );
+}
