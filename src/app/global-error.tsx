b20e6598ad0051d4a3026512ae915fcc59a71546
ethan <wp_wang@manka.co.jp>
2024-10-28 15:20:04 +0900

    init

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -10,9 +10,9 @@ export default function GlobalError({
 }) {
   return (
     <html lang="en">
-      <body className="flex flex-col items-center justify-center h-screen gap-4">
+      <body className="flex flex-col gap-4 p-4">
         <h2 className="text-2xl font-medium">系统维护，请稍后再试！</h2>
-        <Button type="button" onClick={() => reset()}>
+        <Button type="button" variant="link" onClick={() => reset()}>
           重试
         </Button>
       </body>
