8a5b9a2155b7a358035d60ec8633fbe813ccb6de
ethan <wp_wang@manka.co.jp>
2024-10-28 11:13:13 +0900

    init

--- src/api.ts
+++ src/api.ts
@@ -1,70 +1,71 @@
-import { withTimeout } from "es-toolkit/promise";
-import type { AgentData } from "./app/[locale]/(dashboard)/users/agent/page";
+import axios from "axios";
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-const TIMEOUT = 5000;
-
-function validateStatus(status: number): boolean {
-  return status >= 200 && status <= 500;
-}
+const instance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  timeout: 5000,
+  validateStatus(status) {
+    return status >= 200 && status <= 500;
+  },
+});
 
 const FALLBACK_IP_ADDRESS = "0.0.0.0";
 
-export async function request(
-  url: string,
-  nextHeaders?: Headers,
-  config?: RequestInit,
-  token?: string,
-): Promise<Response> {
-  const fullUrl = new URL(url, BASE_URL);
-
-  const headers = new Headers({
-    ...config?.headers,
+export async function request({
+  url,
+  nextHeaders,
+  header,
+  method,
+  data,
+  token,
+}: {
+  url: string;
+  nextHeaders?: Headers;
+  header?: Record<string, string>;
+  method?: string;
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  data?: any;
+  token?: string;
+}) {
+  const headers = {
+    ...header,
     "Content-Type": "application/json",
     "Accept-Language": nextHeaders?.get("Accept-Language") ?? "zh-CN",
-    "time-zone": (new Date().getTimezoneOffset() / 60).toString(),
-  });
+  } as Record<string, string>;
   if (nextHeaders) {
     const array = nextHeaders
       ?.get("x-forwarded-for")
       ?.split(",")[0]
       ?.split(":");
     const ip = array?.[array.length - 1] ?? FALLBACK_IP_ADDRESS;
-    headers.set("X-Forwarded-For", ip);
+    headers["X-Forwarded-For"] = ip;
   }
   if (token) {
-    headers.set("Authorization", `Bearer ${token}`);
+    headers.Authorization = `Bearer ${token}`;
   }
 
-  const response = await withTimeout(() => {
-    return fetch(fullUrl, {
-      ...config,
-      headers,
-    });
-  }, TIMEOUT);
-
-  if (!validateStatus(response.status)) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-
-  return response;
-}
-
-export async function getAgents() {
-  const res = await request("/api/user");
-  return res.json();
-}
-export async function updateUser(data: AgentData) {
-  const res = await request("/api/updateUser", undefined, {
-    method: "PUT",
-    body: JSON.stringify(data),
+  return await instance({
+    url,
+    method,
+    headers,
+    data,
   });
-  return res.json();
 }
-export async function addUser(data: AgentData) {
-  const res = await request("/api/addUser", undefined, {
-    method: "POST",
-    body: JSON.stringify(data),
-  });
-  return res.json();
-}
+
+// export async function getAgents() {
+//   const res = await request("/api/user");
+//   return res.json();
+// }
+// export async function updateUser(data: AgentData) {
+//   const res = await request("/api/updateUser", undefined, {
+//     method: "PUT",
+//     body: JSON.stringify(data),
+//   });
+//   return res.json();
+// }
+// export async function addUser(data: AgentData) {
+//   const res = await request("/api/addUser", undefined, {
+//     method: "POST",
+//     body: JSON.stringify(data),
+//   });
+//   return res.json();
+// }
