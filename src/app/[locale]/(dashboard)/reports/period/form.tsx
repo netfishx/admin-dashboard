5e7490b61ba5bb5eb43bace38a3aa4c57b3e6e85
Emma <emma@123>
2024-10-29 13:37:08 +0800

    fix: 日期组件，样式，固定列

--- src/app/[locale]/(dashboard)/reports/period/form.tsx
+++ src/app/[locale]/(dashboard)/reports/period/form.tsx
@@ -1,9 +1,9 @@
 "use client";
 
+import DateRangeFilter from "@/components/daterange-filter";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import DateFilter from "@/components/date-filter"
 
 import {
   Select,
@@ -21,10 +21,10 @@ export default function Form() {
   const router = useRouter();
   const [gameType, setGameType] = useQueryState("gameType", {
     defaultValue: "1",
-  })
+  });
   const [roomName, setRoomName] = useQueryState("roomName", {
     defaultValue: "1",
-  })
+  });
   const [gameId, setGameId] = useQueryState("gameId", {
     defaultValue: "",
   });
@@ -37,47 +37,56 @@ export default function Form() {
       <div className="flex flex-col gap-2 w-full">
         <div className="bg-background">
           <div className="flex justify-between items-center  py-2 px-4">
-          <div className="flex gap-2 items-center">
             <div className="flex gap-2 items-center">
-              <Label className="shrink-0">{t("gametype")}</Label>
-              <Select value={gameType ?? ""} onValueChange={(value) => setGameType(value)}>
-                <SelectTrigger className="w-28">
-                  <SelectValue placeholder={t("placeholderselect")} />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="1">百家乐</SelectItem>
-                </SelectContent>
-              </Select>
+              <div className="flex gap-2 items-center">
+                <Label className="shrink-0">{t("gametype")}</Label>
+                <Select
+                  value={gameType ?? ""}
+                  onValueChange={(value) => setGameType(value)}
+                >
+                  <SelectTrigger className="w-28">
+                    <SelectValue placeholder={t("placeholderselect")} />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="1">百家乐</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+              <div className="flex gap-2 items-center">
+                <Label className="shrink-0">{t("gamename")}</Label>
+                <Select
+                  value={roomName ?? ""}
+                  onValueChange={(value) => setRoomName(value)}
+                >
+                  <SelectTrigger className="w-28">
+                    <SelectValue placeholder={t("placeholderselect")} />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="1">百家乐01</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+              <div className="flex gap-2 items-center">
+                <Label className="shrink-0">{t("gameId")}</Label>
+                <Input
+                  placeholder={t("placeholderinput")}
+                  value={gameId ?? ""}
+                  onChange={(e) => setGameId(e.target.value)}
+                />
+              </div>
+              <div className="flex gap-2 items-center">
+                <Label className="shrink-0">{t("openTime")}</Label>
+                <DateRangeFilter quickSetBtn={["today", "yesterday"]} />
+              </div>
             </div>
-            <div className="flex gap-2 items-center">
-              <Label className="shrink-0">{t("gamename")}</Label>
-              <Select value={roomName ?? ""} onValueChange={(value) => setRoomName(value)}>
-                <SelectTrigger className="w-28">
-                  <SelectValue placeholder={t("placeholderselect")} />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="1">百家乐01</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
-            <div className="flex gap-2 items-center">
-              <Label className="shrink-0">{t("gameId")}</Label>
-              <Input placeholder={t("placeholderinput")}  value={gameId ?? ""}onChange={(e) => setGameId(e.target.value)}/>
-            </div>
-            <div className="flex gap-2 items-center">
-              <Label className="shrink-0">{t("openTime")}</Label>
-              <DateFilter />
-            </div>
-          </div>
           </div>
           <div className="flex gap-2 items-center float-right p-2">
             <Button variant="outline">{t("reset")}</Button>
             <Button onClick={() => router.refresh()}>{t("search")}</Button>
             <Button variant="outline">{t("download")}</Button>
-          </div>  
-        </div> 
+          </div>
+        </div>
       </div>
-    
     </>
   );
 }
