38ea4374fd926fa61ac7c6a923968ed42fffd59a
gareth <gareth_han@manka.co.jp>
2024-11-01 13:05:21 +0900

    feat: 去除trigger

--- src/app/(dashboard)/reports/agent/baccarat/top-tabs.tsx
+++ src/app/(dashboard)/reports/agent/baccarat/top-tabs.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { times } from "@/components/daterange-filter";
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tabs, TabsList } from "@/components/ui/tabs";
 import { useTranslations } from "next-intl";
 import Link from "next/link";
 import { TabsTypes } from "./defiend";
@@ -10,22 +10,19 @@ export default function TopTabs() {
 
   return (
     <div className="flex flex-col gap-2 bg-background py-2 px-4">
-      <Tabs
-        defaultValue={TabsTypes.RATIO}
-        className="w-[400px]"
-      >
+      <Tabs defaultValue={TabsTypes.RATIO} className="w-[400px]">
         <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value={TabsTypes.RATIO}>
-            <Link href={`/reports/agent/baccarat/ratio?tabsType=${TabsTypes.RATIO}&startTime=${times.startTime}&endTime=${times.endTime}`}>
-              {t("ratioblock")}
-            </Link>
-          </TabsTrigger>
+          <Link
+            href={`/reports/agent/baccarat/ratio?tabsType=${TabsTypes.RATIO}&startTime=${times.startTime}&endTime=${times.endTime}`}
+          >
+            {t("ratioblock")}
+          </Link>
 
-          <TabsTrigger value={TabsTypes.MEMBER}>
-            <Link href={`/reports/agent/baccarat/member?tabsType=${TabsTypes.MEMBER}&startTime=${times.startTime}&endTime=${times.endTime}`}>
-              {t("memberBetting")}
-            </Link>
-          </TabsTrigger>
+          <Link
+            href={`/reports/agent/baccarat/member?tabsType=${TabsTypes.MEMBER}&startTime=${times.startTime}&endTime=${times.endTime}`}
+          >
+            {t("memberBetting")}
+          </Link>
         </TabsList>
       </Tabs>
     </div>
