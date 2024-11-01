bf57861428a9b0b21532c36008392c9bbeb90ac6
gareth <gareth_han@manka.co.jp>
2024-11-01 15:28:16 +0900

    feat: 修改代理报表

--- src/app/(dashboard)/reports/agent/baccarat/top-tabs.tsx
+++ src/app/(dashboard)/reports/agent/baccarat/top-tabs.tsx
@@ -14,7 +14,7 @@ export default function TopTabs() {
   const getTabStyle = (tabType: TabsTypes) => {
     return cn(
       // 基础样式
-      "flex items-center justify-center cursor-pointer text-l",
+      "flex items-center justify-center cursor-pointer text-l rounded-sm",
       // 条件样式
       currentTab === tabType ? "bg-card text-foreground" : "hover:bg-secondary",
     );
