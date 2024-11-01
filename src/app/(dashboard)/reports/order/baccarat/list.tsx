5cf4f7e3bbd35c43304a6f452562c60c0ad7552d
gareth <gareth_han@manka.co.jp>
2024-11-01 14:11:39 +0900

    feat: 修改路径

--- src/app/(dashboard)/reports/order/baccarat/list.tsx
+++ src/app/(dashboard)/reports/order/baccarat/list.tsx
@@ -1,3 +1,4 @@
+import ListScrollArea from "@/components/list-scroll-area";
 import { ScrollBar } from "@/components/ui/scroll-area";
 import {
   Table,
@@ -8,7 +9,6 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { useTranslations } from "next-intl";
-import ListScrollArea from "../../../../../components/list-scroll-area";
 import DetailButton from "./detail-button";
 
 const tempData = "111111111111111111112221";
