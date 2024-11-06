34024d90002814e2945fc8cec3f4829b16991dcb
benjamin <benjamin@gmail.com>
2024-11-06 11:11:20 +0900

    agent edit update

--- src/app/(dashboard)/users/agent/modals.tsx
+++ src/app/(dashboard)/users/agent/modals.tsx
@@ -1,5 +1,11 @@
 import { LoginLogModal } from "./login-log-modal";
+import { UserInfoModal } from "./user-info-modal";
 
 export function Modals() {
-  return <LoginLogModal />;
+  return (
+    <>
+      <LoginLogModal />
+      <UserInfoModal />
+    </>
+  );
 }
