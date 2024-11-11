5244dd89a0aa2b000dc914de75be28de5557153f
gareth <gareth_han@manka.co.jp>
2024-11-11 11:05:15 +0900

    feat: 完成个人中心-基本信息

--- src/app/(dashboard)/personal/info/detail.tsx
+++ src/app/(dashboard)/personal/info/detail.tsx
@@ -1,19 +1,10 @@
-import { getDailiReport } from "@/api";
+import type { UserBasicInfo } from "@/lib/types";
 import { getTranslations } from "next-intl/server";
 import { Suspense } from "react";
 import { DetailButton } from "./detail-button";
 
-export async function Detail({ searchParams }: { searchParams: any }) {
+export async function Detail({ data }: { data: UserBasicInfo }) {
   const t = await getTranslations("personal.info");
-  const search = await searchParams;
-  const { data } = await getDailiReport({
-    ...search,
-    openStartTime: search?.startTime,
-    openEndTime: search?.endTime,
-    page: Number(search?.page ?? 1),
-    size: Number(search?.size ?? 10),
-  });
-
   const formatNumber = (num: number) => {
     return new Intl.NumberFormat("zh-CN", {
       minimumFractionDigits: 2,
@@ -35,19 +26,25 @@ export async function Detail({ searchParams }: { searchParams: any }) {
             <div className="text-muted-foreground text-sm mb-1">
               {t("balance")}
             </div>
-            <div className="text-2xl font-semibold">{formatNumber(112893)}</div>
+            <div className="text-2xl font-semibold">
+              {formatNumber(data?.totalBalanceMoney)}
+            </div>
           </div>
           <div>
             <div className="text-muted-foreground text-sm mb-1">
               {t("availableBalance")}
             </div>
-            <div className="text-2xl font-semibold">{formatNumber(112893)}</div>
+            <div className="text-2xl font-semibold">
+              {formatNumber(data?.usableBalanceMoney)}
+            </div>
           </div>
           <div>
             <div className="text-muted-foreground text-sm mb-1">
               {t("freezeBalance")}
             </div>
-            <div className="text-2xl font-semibold">{formatNumber(112893)}</div>
+            <div className="text-2xl font-semibold">
+              {formatNumber(data?.gameFreezeMoney)}
+            </div>
           </div>
         </div>
 
@@ -57,25 +54,33 @@ export async function Detail({ searchParams }: { searchParams: any }) {
             <div className="text-muted-foreground text-sm mb-1">
               {t("creditAmount")}
             </div>
-            <div className="text-2xl font-semibold">{formatNumber(112893)}</div>
+            <div className="text-2xl font-semibold">
+              {formatNumber(data?.totalCreditMoney)}
+            </div>
           </div>
           <div>
             <div className="text-muted-foreground text-sm mb-1">
               {t("pendingAmount")}
             </div>
-            <div className="text-2xl font-semibold">{formatNumber(112893)}</div>
+            <div className="text-2xl font-semibold">
+              {formatNumber(data?.memberToBeRepaidMoney)}
+            </div>
           </div>
           <div>
             <div className="text-muted-foreground text-sm mb-1">
-              {t("pendingAmount")}
+              {t("unUsedCredit")}
             </div>
-            <div className="text-2xl font-semibold">{formatNumber(112893)}</div>
+            <div className="text-2xl font-semibold">
+              {formatNumber(data?.memberUnusedMoney)}
+            </div>
           </div>
           <div>
             <div className="text-muted-foreground text-sm mb-1">
               {t("withdrawFreezeAmount")}
             </div>
-            <div className="text-2xl font-semibold">{formatNumber(112893)}</div>
+            <div className="text-2xl font-semibold">
+              {formatNumber(data?.withdrawFreezeMoney)}
+            </div>
           </div>
         </div>
       </div>
