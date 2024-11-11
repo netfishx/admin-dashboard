2ae9cbcdee57e8409ac4810ba504b96488ba8db0
gareth <gareth_han@manka.co.jp>
2024-11-11 18:00:28 +0900

    feat: 增加根据用户类型跳转不同url参数

--- src/app/(dashboard)/reports/agent/baccarat/ratio/detail-button.tsx
+++ src/app/(dashboard)/reports/agent/baccarat/ratio/detail-button.tsx
@@ -1,16 +1,18 @@
 "use client";
 import { times } from "@/components/daterange-filter";
 import { Button } from "@/components/ui/button";
+import type { RatioReportListTypes } from "@/lib/types";
 import { useTranslations } from "next-intl";
 import { useRouter } from "next/navigation";
 
-export default function DetailButton(props: { id: string }) {
-  const { id } = props;
+export default function DetailButton(props: { item: RatioReportListTypes }) {
+  const { item } = props;
   const t = useTranslations("report.orderlist");
   const router = useRouter();
+  const ut = item?.userType === 1 ? "agentId" : "houseOwnerId";
   function handleDetail() {
     router.push(
-      `/reports/agent/baccarat/ratio?agentId=${id}&page=1&size=10&startTime=${times.startTime}&endTime=${times.endTime}`,
+      `/reports/agent/baccarat/ratio?${ut}=${item?.id}&page=1&size=10&startTime=${times.startTime}&endTime=${times.endTime}`,
     );
   }
   return (
