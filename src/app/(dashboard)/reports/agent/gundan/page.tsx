25f251e2142d814592a1eafbd7f7837ddabbdc7f
gareth <gareth_han@manka.co.jp>
2024-11-12 16:02:32 +0900

    feat: 修复打包失败

--- src/app/(dashboard)/reports/agent/gundan/page.tsx
+++ src/app/(dashboard)/reports/agent/gundan/page.tsx
@@ -2,13 +2,21 @@ import { Suspense } from "react";
 import { List } from "./list";
 import { ListFilter } from "./list-filter";
 
+function CommonWrapper() {
+  return (
+    <>
+      <ListFilter />
+      <List /> 
+    </>
+  );
+}
+
 export default function GundanPage() {
   return (
     <div className="flex flex-col gap-2 w-full">
       <Suspense fallback={null}>
-        <ListFilter />
+        <CommonWrapper />
       </Suspense>
-      <List />
     </div>
   );
 }
