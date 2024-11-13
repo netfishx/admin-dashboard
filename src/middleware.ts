143e482744347b2c2a231c25191a37b565c46e61
ethan <wp_wang@manka.co.jp>
2024-11-13 13:05:01 +0900

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,7 @@ export async function middleware(request: NextRequest) {
   }
   if (
     user &&
-    !user?.permissions?.includes("agentSta") &&
+    !user?.permissions?.includes("agent") &&
     request.nextUrl.pathname !== "/reports/supplier"
   ) {
     return Response.redirect(new URL("/reports/supplier", request.url));
