21f0aa74bf058b54e096b7f728adcd542dc420f1
Emma <emma@123>
2024-11-14 09:39:46 +0800

    fix: button

--- src/app/(dashboard)/withdraw/audit/actions.tsx
+++ src/app/(dashboard)/withdraw/audit/actions.tsx
@@ -1,76 +1,21 @@
 "use client";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
+
 import { Button } from "@/components/ui/button";
 import type { AuditList } from "@/lib/types";
-import { Loader2 } from "lucide-react";
 import { useTranslations } from "next-intl";
-import { useRouter } from "next/navigation";
-import { useTransition } from "react";
-import { toast } from "sonner";
 
 export function Actions({ data }: { data: AuditList }) {
   const t = useTranslations("withdraw.apply");
   console.info(data);
 
   return (
-    <>
-      <div className="flex justify-center">
-        <LockButton data={data} />
-      </div>
-    </>
-  );
-}
-
-// 锁定
-function LockButton({ data }: { data: AuditList }) {
-  const t = useTranslations("withdraw.apply");
-  const translations = useTranslations();
-  const [isPending, startTransition] = useTransition();
-  const router = useRouter();
-  return (
-    <AlertDialog>
-      <AlertDialogTrigger asChild>
-        <Button variant="link" disabled={isPending} className="px-2">
-          {isPending ? <Loader2 className="w-4 h-4 animate-spin" /> : t("lock")}
-        </Button>
-      </AlertDialogTrigger>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>{t("lockDesc")}</AlertDialogTitle>
-          <AlertDialogDescription />
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel>{translations("cancel")}</AlertDialogCancel>
-          <AlertDialogAction
-            onClick={() => {
-              startTransition(async () => {
-                console.info("锁定提款申请", data.id);
-                const res = await lockApply({
-                  id: data.id,
-                });
-
-                if (res.code === 0) {
-                  router.refresh();
-                } else {
-                  toast.error(res.message);
-                }
-              });
-            }}
-          >
-            {translations("confirm")}
-          </AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
+    <div className="flex justify-center">
+      <Button
+        variant="link"
+        className="hover:no-underline hover:text-primary/80 px-0"
+      >
+        {t("clear")}
+      </Button>
+    </div>
   );
 }
