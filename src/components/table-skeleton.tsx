2011b000c57b854092fb796cca1ad29044db6b43
gareth <gareth_han@manka.co.jp>
2024-11-18 14:14:04 +0900

    修改代码

--- src/components/table-skeleton.tsx
+++ src/components/table-skeleton.tsx
@@ -1,21 +1,13 @@
 import { Skeleton } from "./ui/skeleton";
-import { TableBody, TableCell, TableRow } from "./ui/table";
 
-export default function TableSkeleton(props: {
-  length?: number;
-  colSpan?: number;
-}) {
-  const { length = 5, colSpan = 5 } = props;
+export default function TableSkeleton(props: { length?: number }) {
+  const { length = 5 } = props;
   return (
-    <TableBody>
+    <div className="flex flex-col gap-4 p-4">
       {Array.from({ length }).map((_, i) => (
         // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-        <TableRow key={i}>
-          <TableCell colSpan={colSpan}>
-            <Skeleton className="w-full h-6" />
-          </TableCell>
-        </TableRow>
+        <Skeleton key={i} className="w-full h-6" />
       ))}
-    </TableBody>
+    </div>
   );
 }
