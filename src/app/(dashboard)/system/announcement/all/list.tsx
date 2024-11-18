0edd8b512b92739d46249bacdd67932cdd5b2100
Emma <emma@123>
2024-11-18 17:03:51 +0800

    feat: 本级公告查看

--- src/app/(dashboard)/system/announcement/all/list.tsx
+++ src/app/(dashboard)/system/announcement/all/list.tsx
@@ -18,87 +18,89 @@ export async function List({ data }: { data?: PageData<AnnouncementList> }) {
 
   console.info("data", data);
   return (
-    <div className="border rounded-sm relative overflow-y-auto overflow-x-auto">
-      <Table>
-        <TableHeader>
-          <TableRow className="bg-muted">
-            {/* admin permission */}
-            <TableHead className="w-24 min-w-24 text-center">
-              {t("startTime")}
-            </TableHead>
+    <div>
+      <div className="border rounded-sm">
+        <Table>
+          <TableHeader>
+            <TableRow className="bg-muted">
+              {/* admin permission */}
+              <TableHead className="w-24 min-w-24 text-center">
+                {t("startTime")}
+              </TableHead>
 
-            <TableHead className="w-24 min-w-24 text-center">
-              {t("endTime")}
-            </TableHead>
+              <TableHead className="w-24 min-w-24 text-center">
+                {t("endTime")}
+              </TableHead>
 
-            {/* admin permission */}
-            <TableHead className="w-24 min-w-24 text-center">
-              {t("createTime")}
-            </TableHead>
+              {/* admin permission */}
+              <TableHead className="w-24 min-w-24 text-center">
+                {t("createTime")}
+              </TableHead>
 
-            {/* admin permission */}
-            <TableHead className="w-24 min-w-24 text-center">
-              {t("type")}
-            </TableHead>
+              {/* admin permission */}
+              <TableHead className="w-24 min-w-24 text-center">
+                {t("type")}
+              </TableHead>
 
-            {/* admin permission */}
-            <TableHead className="w-24 min-w-24 text-center">
-              {t("userId")}
-            </TableHead>
+              {/* admin permission */}
+              <TableHead className="w-24 min-w-24 text-center">
+                {t("userId")}
+              </TableHead>
 
-            <TableHead className="w-[450px] min-w-24 text-center">
-              {t("content")}
-            </TableHead>
-            {/* <TableHead className="w-24 min-w-24 text-center">
+              <TableHead className="w-[450px] min-w-24 text-center">
+                {t("content")}
+              </TableHead>
+              {/* <TableHead className="w-24 min-w-24 text-center">
               {t("action")}
             </TableHead> */}
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {data && data.list.length > 0 ? (
-            data.list.map((item) => (
-              <TableRow key={item.id}>
-                {/* admin permission */}
-                <TableCell className="w-24 text-center">
-                  {formatTimestamp(item.startTime)}
-                </TableCell>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {data && data.list.length > 0 ? (
+              data.list.map((item) => (
+                <TableRow key={item.id}>
+                  {/* admin permission */}
+                  <TableCell className="w-24 text-center">
+                    {formatTimestamp(item.startTime)}
+                  </TableCell>
 
-                <TableCell className="w-24 text-center">
-                  {formatTimestamp(item.endTime)}
-                </TableCell>
-                {/* admin permission */}
-                <TableCell className="w-24 text-center">
-                  {formatTimestamp(item.createTime)}
-                </TableCell>
+                  <TableCell className="w-24 text-center">
+                    {formatTimestamp(item.endTime)}
+                  </TableCell>
+                  {/* admin permission */}
+                  <TableCell className="w-24 text-center">
+                    {formatTimestamp(item.createTime)}
+                  </TableCell>
 
-                {/* admin permission */}
-                <TableCell className="w-24 min-w-24 text-center">
-                  {item.type}
-                </TableCell>
+                  {/* admin permission */}
+                  <TableCell className="w-24 min-w-24 text-center">
+                    {item.type}
+                  </TableCell>
 
-                {/* admin permission */}
-                <TableCell className="w-24 min-w-24 text-center">
-                  {item.userId}
-                </TableCell>
+                  {/* admin permission */}
+                  <TableCell className="w-24 min-w-24 text-center">
+                    {item.userId}
+                  </TableCell>
 
-                <TruncatedCell
-                  content={item.contentOfLanguage}
-                  maxLength={50}
-                />
-                {/* <TableCell className="w-24 text-center">
+                  <TruncatedCell
+                    content={item.contentOfLanguage}
+                    maxLength={50}
+                  />
+                  {/* <TableCell className="w-24 text-center">
                   <ViewBtn data={item} />
                 </TableCell> */}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={10} className="text-center h-40">
+                  {translations("noData")}
+                </TableCell>
               </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={10} className="text-center h-40">
-                {translations("noData")}
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
+            )}
+          </TableBody>
+        </Table>
+      </div>
       {Number(data?.total) > 0 && (
         <div className="pt-2">
           <CustomPagination
