b324a196b80a4a49e6b0a6665ab540151894971b
gareth <gareth_han@manka.co.jp>
2024-11-18 15:58:18 +0900

    feat: 完成归集地址管理表格拆分，空状态，骨架屏

--- src/app/(dashboard)/fund/collection/list.tsx
+++ src/app/(dashboard)/fund/collection/list.tsx
@@ -1,4 +1,6 @@
+import { getCollectionAddressList } from "@/api";
 import { DetailButton } from "@/app/(dashboard)/fund/collection/detail-button";
+import TableSkeleton from "@/components/table-skeleton";
 import {
   Table,
   TableBody,
@@ -7,70 +9,74 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import type { CollectionAddressListRecords } from "@/lib/types";
+import type {
+  CollectionAddressListRecords,
+  CollectionAddressListRequestParams,
+} from "@/lib/types";
 import { getTranslations } from "next-intl/server";
 import { Suspense } from "react";
 import CopyButton from "./copy-button";
 
-export async function List({ data }: { data: CollectionAddressListRecords[] }) {
+async function ListHeader() {
+  "use cache";
   const t = await getTranslations("fund.collection");
   return (
-    <Suspense fallback={<div>loading...</div>}>
-      <div className="p-2 bg-background flex-1">
-        <div className="border rounded-sm relative">
-          <Table>
-            <TableHeader>
-              <TableRow className="bg-muted">
-                <TableHead className="min-w-24 text-center">
-                  {t("address")}
-                </TableHead>
-                <TableHead className="min-w-24 text-center">
-                  {t("currency")}
-                </TableHead>
-                <TableHead className="min-w-24 text-center">
-                  {t("balance")}
-                </TableHead>
-                <TableHead className="min-w-24 text-center">
-                  {t("status")}
-                </TableHead>
-                <TableHead className="min-w-24 text-center">
-                  {t("createTime")}
-                </TableHead>
-                <TableHead className="w-24 text-center sticky right-0 z-10 bg-muted">
-                  操作
-                </TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {data?.map((item: CollectionAddressListRecords) => (
-                <TableRow key={item.address}>
-                  <TableCell className="w-32 text-center">
-                    <div className="flex items-center justify-center gap-2">
-                      {item.address}
-                      <CopyButton address={item.address} />
-                    </div>
-                  </TableCell>
-                  <TableCell className="w-32 text-center">
-                    {item.coin}
-                  </TableCell>
-                  <TableCell className="w-36 text-center">
-                    {item.coin}
-                  </TableCell>
-                  <TableCell className="w-36 text-center">
-                    {item.status}
-                  </TableCell>
-                  <TableCell className="w-36 text-center">
-                    {item.updateTime}
-                  </TableCell>
-                  <TableCell className="w-12 text-center sticky right-0 z-10 bg-background">
-                    <DetailButton item={item} />
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </div>
+    <TableHeader>
+      <TableRow className="bg-muted">
+        <TableHead className="min-w-24 text-center">{t("address")}</TableHead>
+        <TableHead className="min-w-24 text-center">{t("currency")}</TableHead>
+        <TableHead className="min-w-24 text-center">{t("balance")}</TableHead>
+        <TableHead className="min-w-24 text-center">{t("status")}</TableHead>
+        <TableHead className="min-w-24 text-center">
+          {t("createTime")}
+        </TableHead>
+        <TableHead className="w-24 text-center sticky right-0 z-10 bg-muted">
+          操作
+        </TableHead>
+      </TableRow>
+    </TableHeader>
+  );
+}
+
+async function ListBody({ list }: { list: CollectionAddressListRecords[] }) {
+  return (
+    <TableBody>
+      {list?.map((item: CollectionAddressListRecords) => (
+        <TableRow key={item.address}>
+          <TableCell className="w-32 text-center">
+            <div className="flex items-center justify-center gap-2">
+              {item.address}
+              <CopyButton address={item.address} />
+            </div>
+          </TableCell>
+          <TableCell className="w-32 text-center">{item.coin}</TableCell>
+          <TableCell className="w-36 text-center">{item.coin}</TableCell>
+          <TableCell className="w-36 text-center">{item.status}</TableCell>
+          <TableCell className="w-36 text-center">{item.updateTime}</TableCell>
+          <TableCell className="w-12 text-center sticky right-0 z-10 bg-background">
+            <DetailButton item={item} />
+          </TableCell>
+        </TableRow>
+      ))}
+    </TableBody>
+  );
+}
+
+export async function List({
+  searchParams,
+}: { searchParams: Promise<CollectionAddressListRequestParams> }) {
+  const params = await searchParams;
+  const { data } = await getCollectionAddressList(params);
+  return (
+    <div className="p-2 bg-background flex-1">
+      <div className="border rounded-sm relative">
+        <Table>
+          <ListHeader />
+          <Suspense fallback={<TableSkeleton length={5} colSpan={10} />}>
+            <ListBody list={data ?? []} />
+          </Suspense>
+        </Table>
       </div>
-    </Suspense>
+    </div>
   );
 }
