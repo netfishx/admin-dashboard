bc88a7c91aca188a97b7b82d29a085acc0231509
gareth <gareth_han@manka.co.jp>
2024-11-18 13:35:28 +0900

    feat: 完成tab样式修改

--- src/app/(dashboard)/reports/order/baccarat/list.tsx
+++ src/app/(dashboard)/reports/order/baccarat/list.tsx
@@ -16,6 +16,7 @@ import DetailButton from "./detail-button";
 
 export function List({ data }: { data: PageData<OrderReportsRecord> }) {
   const t = useTranslations("report.orderlist");
+  const translate = useTranslations();
   return (
     <div className="p-2 bg-background flex-1">
       <div className="border rounded-sm relative">
@@ -71,57 +72,67 @@ export function List({ data }: { data: PageData<OrderReportsRecord> }) {
               </TableRow>
             </TableHeader>
             <TableBody>
-              {data?.list?.map((item: OrderReportsRecord) => (
-                <TableRow key={item.id}>
-                  <TableCell className="w-24 text-center">{item.id}</TableCell>
-                  <TableCell className="w-24 text-center">
-                    {item.issueNumber}
-                  </TableCell>
-                  <TableCell className="w-24 text-center">
-                    {item.memberId}
-                  </TableCell>
-                  <TableCell className="w-24 text-center">
-                    {item.roomOwnerId}
-                  </TableCell>
-                  <TableCell className="w-24 text-center">
-                    {item.minister}
-                  </TableCell>
-                  <TableCell className="w-24 text-center">
-                    {item.lastAgentId}
-                  </TableCell>
-                  <TableCell className="w-24 text-center">
-                    {item.gameId}
-                  </TableCell>
-                  <TableCell className="w-24 text-center">
-                    {item.betType}
-                  </TableCell>
-                  <TableCell className="w-24 text-center">
-                    {
-                      Object.entries(item.odds || {})[
-                        Object.entries(item.odds || {}).length - 1
-                      ]
-                    }
-                  </TableCell>
-                  <TableCell className="w-24 text-center">
-                    {item?.betAmount}
-                  </TableCell>
-                  <TableCell className="w-24 text-center">
-                    {item.winLossAmount}
-                  </TableCell>
-                  <TableCell className="text-center">
-                    {format(item.betTime, "yyyy-MM-dd HH:mm:ss")}
-                  </TableCell>
-                  <TableCell className="text-center">
-                    {format(item.settleTime, "yyyy-MM-dd HH:mm:ss")}
-                  </TableCell>
-                  <TableCell className="w-24 text-center">
-                    {item.orderStatus}
-                  </TableCell>
-                  <TableCell className="w-24 text-center sticky right-0 z-10 bg-background">
-                    <DetailButton />
+              {data?.list?.length > 0 ? (
+                data?.list?.map((item: OrderReportsRecord) => (
+                  <TableRow key={item.id}>
+                    <TableCell className="w-24 text-center">
+                      {item.id}
+                    </TableCell>
+                    <TableCell className="w-24 text-center">
+                      {item.issueNumber}
+                    </TableCell>
+                    <TableCell className="w-24 text-center">
+                      {item.memberId}
+                    </TableCell>
+                    <TableCell className="w-24 text-center">
+                      {item.roomOwnerId}
+                    </TableCell>
+                    <TableCell className="w-24 text-center">
+                      {item.minister}
+                    </TableCell>
+                    <TableCell className="w-24 text-center">
+                      {item.lastAgentId}
+                    </TableCell>
+                    <TableCell className="w-24 text-center">
+                      {item.gameId}
+                    </TableCell>
+                    <TableCell className="w-24 text-center">
+                      {item.betType}
+                    </TableCell>
+                    <TableCell className="w-24 text-center">
+                      {
+                        Object.entries(item.odds || {})[
+                          Object.entries(item.odds || {}).length - 1
+                        ]
+                      }
+                    </TableCell>
+                    <TableCell className="w-24 text-center">
+                      {item?.betAmount}
+                    </TableCell>
+                    <TableCell className="w-24 text-center">
+                      {item.winLossAmount}
+                    </TableCell>
+                    <TableCell className="text-center">
+                      {format(item.betTime, "yyyy-MM-dd HH:mm:ss")}
+                    </TableCell>
+                    <TableCell className="text-center">
+                      {format(item.settleTime, "yyyy-MM-dd HH:mm:ss")}
+                    </TableCell>
+                    <TableCell className="w-24 text-center">
+                      {item.orderStatus}
+                    </TableCell>
+                    <TableCell className="w-24 text-center sticky right-0 z-10 bg-background">
+                      <DetailButton />
+                    </TableCell>
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell colSpan={15} className="text-center h-40">
+                    {translate("noData")}
                   </TableCell>
                 </TableRow>
-              ))}
+              )}
             </TableBody>
           </Table>
           <ScrollBar orientation="horizontal" />
