b6f2cfd2df301de5f1968149dbfc5d60768f67fd
gareth <gareth_han@manka.co.jp>
2024-11-18 15:13:28 +0900

    feat: 代理报表-真人视讯-占城拦货表格拆分,空状态，骨架屏

--- src/app/(dashboard)/reports/agent/baccarat/ratio/page.tsx
+++ src/app/(dashboard)/reports/agent/baccarat/ratio/page.tsx
@@ -1,6 +1,6 @@
-import { getRatioReport } from "@/api";
 import TableSkeleton from "@/components/table-skeleton";
-import type { PageData, RatioReportRequestRecords } from "@/lib/types";
+import { Skeleton } from "@/components/ui/skeleton";
+import type { RatioReportRequestRecords } from "@/lib/types";
 import { Suspense } from "react";
 import { RatioForm } from "./ratio-form";
 import { RatioList } from "./ratio-list";
@@ -9,15 +9,19 @@ interface CommonWrapperProps {
 }
 
 async function CommonWrapper({ searchParams }: CommonWrapperProps) {
-  const params = await searchParams;
-  const { data } = await getRatioReport(params);
   return (
     <>
-      <Suspense fallback={null}>
+      <Suspense
+        fallback={
+          <div className="flex justify-between items-center bg-background p-4">
+            <Skeleton className="w-full h-9 opacity-20" />
+          </div>
+        }
+      >
         <RatioForm />
       </Suspense>
-      <Suspense fallback={<TableSkeleton length={5} />}>
-        <RatioList data={data as PageData<RatioReportRequestRecords>} />
+      <Suspense fallback={<TableSkeleton length={5} colSpan={11} />}>
+        <RatioList searchParams={searchParams} />
       </Suspense>
     </>
   );
--- src/app/(dashboard)/reports/agent/baccarat/ratio/ratio-list.tsx
+++ src/app/(dashboard)/reports/agent/baccarat/ratio/ratio-list.tsx
@@ -1,5 +1,7 @@
+import { getRatioReport } from "@/api";
 import DetailButton from "@/app/(dashboard)/reports/agent/baccarat/ratio/detail-button";
 import { CustomPagination } from "@/components/custom-pagination";
+import TableSkeleton from "@/components/table-skeleton";
 import {
   Table,
   TableBody,
@@ -8,102 +10,112 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import type { PageData, RatioReportRequestRecords } from "@/lib/types";
+import type { RatioReportRequestRecords } from "@/lib/types";
 import { getTranslations } from "next-intl/server";
+import { Suspense } from "react";
+
+async function ListHeader() {
+  "use cache";
+  const t = await getTranslations("report.agent");
+  return (
+    <TableHeader>
+      <TableRow className="bg-muted">
+        <TableHead className="min-w-24 text-center">
+          {t("agentOrOwnerId")}
+        </TableHead>
+        <TableHead className="min-w-24 text-center">{t("gameName")}</TableHead>
+        <TableHead className="min-w-24 text-center">
+          {t("shareAmount")}
+        </TableHead>
+        <TableHead className="min-w-24 text-center">
+          {t("blockAmount")}
+        </TableHead>
+        <TableHead className="min-w-24 text-center">
+          {t("throwAmount")}
+        </TableHead>
+        <TableHead className="min-w-24 text-center">
+          {t("shareProfitLoss")}
+        </TableHead>
+        <TableHead className="min-w-24 text-center">
+          {t("rebateIncome")}
+        </TableHead>
+        <TableHead className="min-w-24 text-center">
+          {t("rebateExpense")}
+        </TableHead>
+        <TableHead className="min-w-24 text-center">{t("netRebate")}</TableHead>
+        <TableHead className="min-w-24 text-center">
+          {t("totalProfitLossAmount")}
+        </TableHead>
+        <TableHead className="w-24 text-center sticky right-0 z-10 bg-muted">
+          {t("more")}
+        </TableHead>
+      </TableRow>
+    </TableHeader>
+  );
+}
+
+async function ListBody({ list }: { list: RatioReportRequestRecords[] }) {
+  const translate = await getTranslations();
+  return (
+    <TableBody>
+      {list?.length > 0 ? (
+        list?.map((item: RatioReportRequestRecords) => (
+          <TableRow key={`${item.userId}`}>
+            <TableCell className="w-24 text-center">{item.userId}</TableCell>
+            <TableCell className="w-24 text-center">{item.gameName}</TableCell>
+            <TableCell className="w-24 text-center">
+              {item.expectedShareAmount}
+            </TableCell>
+            <TableCell className="w-24 text-center">
+              {item.interceptAmount}
+            </TableCell>
+            <TableCell className="w-24 text-center">
+              {item.throwAmount}
+            </TableCell>
+            <TableCell className="w-24 text-center">
+              {item.actualShareWinLoss}
+            </TableCell>
+            <TableCell className="w-24 text-center">
+              {item.backIncome}
+            </TableCell>
+            <TableCell className="w-24 text-center">
+              {item.backOutcome}
+            </TableCell>
+            <TableCell className="w-24 text-center">
+              {item.pureBackAmount}
+            </TableCell>
+            <TableCell className="w-24 text-center">
+              {item.totalProfitLossAmount}
+            </TableCell>
+            <TableCell className="w-24 text-center sticky right-0 z-10 bg-background">
+              <DetailButton item={item} />
+            </TableCell>
+          </TableRow>
+        ))
+      ) : (
+        <TableRow>
+          <TableCell colSpan={15} className="text-center h-40">
+            {translate("noData")}
+          </TableCell>
+        </TableRow>
+      )}
+    </TableBody>
+  );
+}
 
 export async function RatioList({
-  data,
-}: { data: PageData<RatioReportRequestRecords> }) {
-  const t = await getTranslations("report.agent");
-  const translate = await getTranslations();
+  searchParams,
+}: { searchParams: Promise<RatioReportRequestRecords> }) {
+  const params = await searchParams;
+  const { data } = await getRatioReport(params);
   return (
     <div className="p-2 bg-background flex-1">
       <div className="border rounded-sm relative">
         <Table>
-          <TableHeader>
-            <TableRow className="bg-muted">
-              <TableHead className="min-w-24 text-center">
-                {t("agentOrOwnerId")}
-              </TableHead>
-              <TableHead className="min-w-24 text-center">
-                {t("gameName")}
-              </TableHead>
-              <TableHead className="min-w-24 text-center">
-                {t("shareAmount")}
-              </TableHead>
-              <TableHead className="min-w-24 text-center">
-                {t("blockAmount")}
-              </TableHead>
-              <TableHead className="min-w-24 text-center">
-                {t("throwAmount")}
-              </TableHead>
-              <TableHead className="min-w-24 text-center">
-                {t("shareProfitLoss")}
-              </TableHead>
-              <TableHead className="min-w-24 text-center">
-                {t("rebateIncome")}
-              </TableHead>
-              <TableHead className="min-w-24 text-center">
-                {t("rebateExpense")}
-              </TableHead>
-              <TableHead className="min-w-24 text-center">
-                {t("netRebate")}
-              </TableHead>
-              <TableHead className="min-w-24 text-center">
-                {t("totalProfitLossAmount")}
-              </TableHead>
-              <TableHead className="w-24 text-center sticky right-0 z-10 bg-muted">
-                {t("more")}
-              </TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {data?.list?.length > 0 ? (
-              data?.list?.map((item: RatioReportRequestRecords) => (
-                <TableRow key={`${item.userId}`}>
-                  <TableCell className="w-24 text-center">
-                    {item.userId}
-                  </TableCell>
-                  <TableCell className="w-24 text-center">
-                    {item.gameName}
-                  </TableCell>
-                  <TableCell className="w-24 text-center">
-                    {item.expectedShareAmount}
-                  </TableCell>
-                  <TableCell className="w-24 text-center">
-                    {item.interceptAmount}
-                  </TableCell>
-                  <TableCell className="w-24 text-center">
-                    {item.throwAmount}
-                  </TableCell>
-                  <TableCell className="w-24 text-center">
-                    {item.actualShareWinLoss}
-                  </TableCell>
-                  <TableCell className="w-24 text-center">
-                    {item.backIncome}
-                  </TableCell>
-                  <TableCell className="w-24 text-center">
-                    {item.backOutcome}
-                  </TableCell>
-                  <TableCell className="w-24 text-center">
-                    {item.pureBackAmount}
-                  </TableCell>
-                  <TableCell className="w-24 text-center">
-                    {item.totalProfitLossAmount}
-                  </TableCell>
-                  <TableCell className="w-24 text-center sticky right-0 z-10 bg-background">
-                    <DetailButton item={item} />
-                  </TableCell>
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell colSpan={15} className="text-center h-40">
-                  {translate("noData")}
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
+          <ListHeader />
+          <Suspense fallback={<TableSkeleton length={5} />}>
+            <ListBody list={data?.list ?? []} />
+          </Suspense>
         </Table>
       </div>
       <div className="pt-2">
