727a6371ba73783ea1dcb4dbbf2266acb8a0fd65
ethan <wp_wang@manka.co.jp>
2024-12-03 17:20:38 +0900

    fix bug

--- src/app/login/form.tsx
+++ src/app/login/form.tsx
@@ -46,7 +46,7 @@ export function LoginForm() {
   }
   const [code, setCode] = useState(nanoid());
   return (
-    <Form action={loginAction} onSubmit={handleSubmit} ref={ref}>
+    <Form action="" onSubmit={handleSubmit} ref={ref}>
       <div className="relative w-full h-screen overflow-hidden bg-accent flex flex-col gap-4 items-center justify-center">
         <Image src={bg} alt="background image" className="object-cover" fill />
         <div className="w-[400px] flex items-center justify-center gap-4">
