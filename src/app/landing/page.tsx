8cc111a687dc64b41e1221a32cb3eb28e120a604
ethan <wp_wang@manka.co.jp>
2024-12-05 14:32:51 +0900

    倒计时

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,7 +1,10 @@
+import { Time } from "./time";
+
 export default async function LandingPage() {
   return (
     <div className="prose">
       <h1>LandingPage</h1>
+      <Time />
     </div>
   );
 }
