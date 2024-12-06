6b94615fff2e0ff6fff1b922a76097ba613871a7
gareth <gareth_han@manka.co.jp>
2024-12-06 11:24:04 +0900

    feat: 修复打包错误

--- src/api.ts
+++ src/api.ts
@@ -16,6 +16,7 @@ import type {
   BorrowRecordRequestRecords,
   ChangeLog,
   CollectionAddressListRecords,
+  CreditRecordRequestParams,
   CreditRecordRequestRecords,
   DictionaryItemList,
   DictionaryList,
