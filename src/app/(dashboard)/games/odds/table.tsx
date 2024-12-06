93b5863e71d339a71be65f41ccb3303111c8a145
ethan <wp_wang@manka.co.jp>
2024-12-06 15:18:57 +0900

    fix bug

--- src/app/(dashboard)/games/odds/table.tsx
+++ src/app/(dashboard)/games/odds/table.tsx
@@ -152,7 +152,7 @@ export function OddsTable({
                   }}
                 />
                 {item.maxBetLimit ? (
-                  <span className="text-destructive w-20">
+                  <span className="text-destructive w-20 shrink-0">
                     ({item.maxBetLimit})
                   </span>
                 ) : null}
@@ -184,7 +184,7 @@ export function OddsTable({
                   }}
                 />
                 {item.maxBetPeriodLimit ? (
-                  <span className="text-destructive w-20">
+                  <span className="text-destructive w-20 shrink-0">
                     ({item.maxBetPeriodLimit})
                   </span>
                 ) : null}
