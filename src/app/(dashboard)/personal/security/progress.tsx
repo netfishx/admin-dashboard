b6a4239a4e31f36b3e7e2ee69f82152a802e1e18
cricket <null>
2024-12-11 18:07:23 +0900

    fix bug

--- src/app/(dashboard)/personal/security/progress.tsx
+++ src/app/(dashboard)/personal/security/progress.tsx
@@ -1,22 +1,35 @@
+import { Skeleton } from "@/components/ui/skeleton";
 import { useTranslations } from "next-intl";
 import styles from "./progress.module.css";
 
-export function SecurityProgress({ value }: { value: number }) {
+export function SecurityProgress({ value }: { value?: number }) {
   const t = useTranslations("personal.security");
 
   return (
-    <div className="bg-background py-2 px-4 flex flex-col gap-2">
+    <div className="bg-background p-4 flex flex-col gap-2">
       <span className="text-sm font-medium">{t("progress")}</span>
       <div className="flex items-center gap-2">
-        <progress value={value} max={100} className={styles.progress} />
-        <span className="text-sm text-muted-foreground">{value}%</span>
+        {value ? (
+          <>
+            <progress value={value} max={100} className={styles.progress} />
+            <span className="text-sm text-muted-foreground">{value}%</span>
+          </>
+        ) : (
+          <Skeleton className="w-1/2" />
+        )}
       </div>
       <span className="text-sm text-muted-foreground">
-        {value >= 99
-          ? t("progressWarning3")
-          : value >= 66
-            ? t("progressWarning2")
-            : t("progressWarning")}
+        {value ? (
+          value >= 99 ? (
+            t("progressWarning3")
+          ) : value >= 66 ? (
+            t("progressWarning2")
+          ) : (
+            t("progressWarning")
+          )
+        ) : (
+          <Skeleton className="w-1/3" />
+        )}
       </span>
     </div>
   );
