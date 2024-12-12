76035fdab4b2f84c293d4fc09ba02486500acd6b
cricket <null>
2024-12-12 13:00:23 +0900

    fix bug

--- src/app/(dashboard)/games/maintain/form.tsx
+++ src/app/(dashboard)/games/maintain/form.tsx
@@ -46,7 +46,7 @@ function BatchButton({
         <AlertDialogHeader>
           <AlertDialogTitle>
             {translations("title", {
-              status: translations(status === 0 ? "open" : "close"),
+              status: translations(status === 1 ? "open" : "close"),
             })}
           </AlertDialogTitle>
           <AlertDialogDescription />
@@ -81,8 +81,8 @@ export function MaintainForm() {
   return (
     <div className="flex items-center justify-end gap-2 bg-background p-4">
       <Suspense>
-        <BatchButton status={0}>{t("batchClose")}</BatchButton>
         <BatchButton status={1}>{t("batchOpen")}</BatchButton>
+        <BatchButton status={0}>{t("batchClose")}</BatchButton>
       </Suspense>
     </div>
   );
