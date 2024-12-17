335f4f60f5402abb65fadc92e1cf71b9cb9d2f52
cricket <null>
2024-12-17 15:57:08 +0900

    fix bug

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,25 +1,22 @@
-import React from "react";
+import type { InputHTMLAttributes, RefAttributes } from "react";
 
 import { cn } from "@/lib/utils";
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
-
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-xs transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-hidden focus-visible:ring focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 disabled:bg-muted",
-          className,
-        )}
-        ref={ref}
-        {...props}
-      />
-    );
-  },
-);
-Input.displayName = "Input";
-
-export { Input };
+export function Input({
+  className,
+  type,
+  ref,
+  ...props
+}: InputHTMLAttributes<HTMLInputElement> & RefAttributes<HTMLInputElement>) {
+  return (
+    <input
+      type={type}
+      className={cn(
+        "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-xs transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-hidden focus-visible:ring focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 disabled:bg-muted",
+        className,
+      )}
+      ref={ref}
+      {...props}
+    />
+  );
+}
