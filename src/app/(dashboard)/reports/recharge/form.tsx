6e25c302955cbe15986ccb3649145f5d0fab5b45
Emma <emma@123>
2024-12-24 15:19:17 +0800

    充值提现记录加快捷选择

--- src/app/(dashboard)/reports/recharge/form.tsx
+++ src/app/(dashboard)/reports/recharge/form.tsx
@@ -66,7 +66,7 @@ export function Form() {
       <div className="flex items-center gap-4">
         <div className="flex items-center gap-2">
           <Label className="shrink-0">{t("finishTime")}</Label>
-          <DateRangeFilter quickSetBtn={[]} />
+          <DateRangeFilter />
         </div>
         <div className="flex items-center gap-2">
           <Label className="shrink-0">{t("orderNo")}</Label>
