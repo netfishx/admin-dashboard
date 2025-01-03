86ed9013ebf319f7af6d4e1e6e8e1ffd09213228
ethan <ethan@example.com>
2025-01-03 13:35:30 +0900

    fix bug

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 
 function LinkButton({ children, href }: { children: ReactNode; href: string }) {
   return (
-    <Button asChild className="w-36 bg-[#3590ec] hover:bg-[#3590ec]/80">
+    <Button asChild className="w-36 bg-blue-500/90 hover:bg-blue-500/80">
       <Link href={href} target="_blank">
         {children}
       </Link>
@@ -24,12 +24,10 @@ export default async function LandingPage() {
     <div>
       <Bg />
       <div className="absolute bottom-[10dvh] z-10 flex w-full items-center justify-center gap-4">
-        <LinkButton href="https://ui.shadcn.com/docs/components/button">
+        <LinkButton href="https://006test.s3.ap-east-1.amazonaws.com/006test/apk/SIX_0102.apk">
           安卓下载
         </LinkButton>
-        <LinkButton href="https://ui.shadcn.com/docs/components/button">
-          iOS下载
-        </LinkButton>
+        <LinkButton href="https://google.com">iOS下载</LinkButton>
       </div>
     </div>
   );
