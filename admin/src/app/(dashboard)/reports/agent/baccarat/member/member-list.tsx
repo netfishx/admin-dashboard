c9396a1fd6d56f5c20e5103d610bab1281fcaf71
benjamin <benjamin@123>
2025-01-06 16:23:53 +0800

    Feat fix type bug

--- admin/src/app/(dashboard)/reports/agent/baccarat/member/member-list.tsx
+++ admin/src/app/(dashboard)/reports/agent/baccarat/member/member-list.tsx
@@ -33,8 +33,8 @@ export async function ListHeader() {
         <TableHead className="w-32">{t("shareAmount")}</TableHead>
         <TableHead className="w-32">{t("blockAmount")}</TableHead>
         <TableHead className="w-32">{t("throwAmount")}</TableHead>
-        <TableHead className="w-32">{t("shareProfitLoss")}</TableHead>
         <TableHead className="w-32">{t("actualShareAmount")}</TableHead>
+        <TableHead className="w-32">{t("actualShareWinLoss")}</TableHead>
         <TableHead className="w-32">{t("rebateIncome")}</TableHead>
         <TableHead className="w-32">{t("rebateExpense")}</TableHead>
         <TableHead className="w-32">{t("netRebate")}</TableHead>
@@ -82,6 +82,9 @@ async function ListBody({
               {formatNumber(Number(item.interceptAmount || 0))}
             </TableCell>
             <TableCell>{formatNumber(Number(item.throwAmount || 0))}</TableCell>
+            <TableCell>
+              {formatNumber(Number(item.actualShareAmount || 0))}
+            </TableCell>
             <TableCell>
               {formatNumber(Number(item.actualShareWinLoss || 0))}
             </TableCell>
