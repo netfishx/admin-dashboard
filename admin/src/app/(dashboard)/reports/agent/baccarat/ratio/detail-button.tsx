dcaeebec5d895cc809fb87afcaf0ea4e9faea98d
cricket <your_email@example.com>
2025-01-13 17:52:32 +0900

    fix bug

--- admin/src/app/(dashboard)/reports/agent/baccarat/ratio/detail-button.tsx
+++ admin/src/app/(dashboard)/reports/agent/baccarat/ratio/detail-button.tsx
@@ -1,30 +1,34 @@
-"use client";
 import { Button } from "@/components/ui/button";
 import type { RatioReportListTypes } from "@/lib/types";
 import { useTranslations } from "next-intl";
-import { useTransitionRouter } from "next-view-transitions";
-import { useSearchParams } from "next/navigation";
-export default function DetailButton(props: { item: RatioReportListTypes }) {
-  const { item } = props;
-  const searchParams = useSearchParams();
-  const gameId = searchParams.get("gameId");
-  const startTime = searchParams.get("startTime");
-  const endTime = searchParams.get("endTime");
+import Link from "next/link";
+
+export default function DetailButton({
+  item,
+  searchParams,
+}: {
+  item: RatioReportListTypes;
+  searchParams: { [key: string]: string | undefined };
+}) {
   const t = useTranslations("report.orderlist");
-  const router = useTransitionRouter();
-  function handleDetail() {
-    router.push(
-      `/reports/agent/baccarat/ratio?${gameId ? `gameId=${gameId}&` : ""}parentAgentId=${item?.userId}&startTime=${startTime}&endTime=${endTime}`,
-    );
-  }
   return (
     <div>
       <Button
         variant="ghost"
         className="text-primary hover:text-primary/80 hover:no-underline"
-        onClick={() => handleDetail()}
+        asChild
       >
-        {t("more")}
+        <Link
+          href={{
+            pathname: "/reports/agent/baccarat/ratio",
+            query: {
+              ...searchParams,
+              parentAgentId: item?.userId,
+            },
+          }}
+        >
+          {t("more")}
+        </Link>
       </Button>
     </div>
   );
