501d4d3f8d11b0be68ccdc5f926f88f1ae00e6c9
cricket <your_email@example.com>
2025-01-14 09:20:59 +0900

    fix bug

--- admin/src/app/(dashboard)/reports/agent/baccarat/defiend.ts
+++
@@ -1,4 +0,0 @@
-export enum TabsTypes {
-  RATIO = "ratio",
-  MEMBER = "member",
-}
--- admin/src/app/(dashboard)/reports/agent/baccarat/top-tabs.tsx
+++ admin/src/app/(dashboard)/reports/agent/baccarat/top-tabs.tsx
@@ -1,48 +1,39 @@
 "use client";
-import { TabsTypes } from "@/app/(dashboard)/reports/agent/baccarat/defiend";
+
 import { Tabs, TabsList } from "@/components/ui/tabs";
-import { cn } from "@/lib/utils";
 import { useTranslations } from "next-intl";
 import { Link } from "next-view-transitions";
-import { useState } from "react";
+import { usePathname } from "next/navigation";
 
 export default function TopTabs() {
   const t = useTranslations("report.agent");
-  const [currentTab, setCurrentTab] = useState(TabsTypes.RATIO);
-
-  const getTabStyle = (tabType: TabsTypes) => {
-    return cn(
-      // 基础样式
-      "inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 w-full",
-      // 条件样式
-      currentTab === tabType
-        ? "bg-card text-foreground shadow-sm"
-        : "hover:bg-secondary",
-    );
-  };
-
+  const pathname = usePathname();
   return (
     <div className="bg-background flex flex-col gap-2 p-4">
-      <Tabs defaultValue={TabsTypes.RATIO} className="w-[400px]">
+      <Tabs className="w-[400px]">
         <TabsList className="grid w-full grid-cols-2">
-          <Link href="/reports/agent/baccarat/ratio">
-            {/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
-            <div
-              className={getTabStyle(TabsTypes.RATIO)}
-              onClick={() => setCurrentTab(TabsTypes.RATIO)}
-            >
-              {t("ratioblock")}
-            </div>
+          <Link
+            data-state={
+              pathname === "/reports/agent/baccarat/ratio"
+                ? "active"
+                : "inactive"
+            }
+            href="/reports/agent/baccarat/ratio"
+            className="focus-visible:outline-hidden focus-visible:ring-ring data-[state=active]:bg-background data-[state=active]:text-foreground inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium transition-all focus-visible:ring disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm"
+          >
+            {t("ratioblock")}
           </Link>
 
-          <Link href="/reports/agent/baccarat/member">
-            {/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
-            <div
-              className={getTabStyle(TabsTypes.MEMBER)}
-              onClick={() => setCurrentTab(TabsTypes.MEMBER)}
-            >
-              {t("memberBetting")}
-            </div>
+          <Link
+            data-state={
+              pathname === "/reports/agent/baccarat/member"
+                ? "active"
+                : "inactive"
+            }
+            href="/reports/agent/baccarat/member"
+            className="focus-visible:outline-hidden focus-visible:ring-ring data-[state=active]:bg-background data-[state=active]:text-foreground inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium transition-all focus-visible:ring disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm"
+          >
+            {t("memberBetting")}
           </Link>
         </TabsList>
       </Tabs>
