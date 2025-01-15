6925320a5514b5e901fa801a0f79667d77e12505
benjamin <benjamin@gmail.com>
2025-01-15 10:09:42 +0900

    注单 百家乐 占成竖向展示

--- admin/src/app/(dashboard)/reports/order/baccarat/detail-dialog.tsx
+++ admin/src/app/(dashboard)/reports/order/baccarat/detail-dialog.tsx
@@ -62,18 +62,18 @@ export function Detaildialog(props: {
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="max-w-4xl">
+      <DialogContent className="max-w-3xl">
         <DialogHeader>
           <DialogTitle>{t("detail")}</DialogTitle>
         </DialogHeader>
         <div className="bg-muted text-muted-foreground rounded-sm p-2 text-center font-medium">
           {t("shareDetail")}
         </div>
-        <ScrollArea className="w-[846px]">
-          <div className="mb-1 whitespace-nowrap text-center">
+        <ScrollArea className="w-[718px]">
+          <div className="mb-1 text-center flex flex-col gap-1">
             {data?.revenueShare.map(
               (item) =>
-                `${item.accountId} - ${Big(item.percent * 100).toFixed(2)}%；`,
+                <span key={item.accountId}>{item.accountId} {Big(item.percent * 100).toFixed(2)}%</span>
             )}
           </div>
           <ScrollBar orientation="horizontal" />
@@ -84,7 +84,7 @@ export function Detaildialog(props: {
             <TableRow className="bg-muted">
               <TableHead className="w-12 text-center">{t("shoe")}</TableHead>
               <TableHead className="w-12 text-center">{t("play")}</TableHead>
-              <TableHead className="w-40 text-center">{t("resultp")}</TableHead>
+              <TableHead className="w-36 text-center">{t("resultp")}</TableHead>
             </TableRow>
           </TableHeader>
 
